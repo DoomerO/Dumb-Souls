b060ddb269ee8852bc7703e8cfda8e1d557d4a95
Doomer_O <109368541+DoomerO@users.noreply.github.com>
2022-12-31 14:28:23 -0300

    Add files via upload

--- DumbSouls/src/entities/Boss_Duality.java
+++ DumbSouls/src/entities/Boss_Duality.java
@@ -10,10 +10,12 @@ import java.awt.Graphics;
 
 public class Boss_Duality extends Enemy{
 	
-	private int frames, maxFrames = 40, index, maxIndex = 3, timeAtk ;
-	private boolean balance;
+	private int frames, maxFrames = 40, index, maxIndex = 3, timeAtk, shieldLife;
+	private boolean balance, shieldActive;
 	private BufferedImage aura = Game.sheet.getSprite(16, 160, 16, 16);
+	private BufferedImage shield = Game.sheet.getSprite(16, 176, 16, 16);
 	private BufferedImage spriteAtk = Game.sheet.getSprite(32, 160, 16, 16);
+	private BufferedImage spriteAtk2 = Game.sheet.getSprite(32, 176, 16, 16);
 	
 	public Boss_Duality(int x, int y, int width, int height, BufferedImage sprite) {
 		super(x, y, width, height, sprite);
@@ -46,19 +48,44 @@ public class Boss_Duality extends Enemy{
 	}
 	
 	private void die() {
-		
 		Game.enemies.remove(this);
 		Game.player.exp += this.expValue;
 		Player.souls +=  this.soulValue;
 	}
+	
 	private void closeAtk() {
 		if (Entity.calculateDistance(Game.player.getX(), Game.player.getY(), this.getX() + 16, this.getY() + 16) <= 60) {
 			double ang = Math.atan2((Game.player.getY() - Camera.y) - (this.getY() - Camera.y) ,(Game.player.getX() - Camera.x) - (this.getX() - Camera.x));
 			double dx = Math.cos(ang);
 			double dy =  Math.sin(ang);
 
-			Game.eShots.add(new Enemy_Shot(this.getX() + 3, this.getY() + 11, 6, 3, spriteAtk, dx, dy, 15, 2, 30));
+			Game.eShots.add(new Enemy_Shot(this.getX() + 3, this.getY() + 11, 6, 3, spriteAtk, dx, dy, 15, 2, 30, "straight"));
 			Game.player.mana *= 0.75;
+		}
+	}
+	
+	private void shieldColision() {
+		for (int i = 0;  i < Game.shots.size(); i++) {
+			Shot e = Game.shots.get(i);
+			if (isColiding(this, e)) {
+				e.die();
+			}
+		}
+		
+		if (this.life <= shieldLife) {
+			this.life = shieldLife;
+		}
+	}
+	
+	private void addShield() {
+		if (!shieldActive) {
+			shieldActive = true;
+			shieldLife = (int)this.life;
+			this.setMask(0, 0, 98, 64);
+		}
+		else {
+			shieldActive = false;
+			this.setMask(11, 6, 12, 20);
 		}
 	}
 	
@@ -72,13 +99,13 @@ public class Boss_Duality extends Enemy{
 			double dx = Math.cos(ang);
 			double dy =  Math.sin(ang);
 
-			Game.eShots.add(new Enemy_Shot(this.getX() + 3, this.getY() + 11, 6, 3, spriteAtk, dx, dy, 5, 5, 35));
+			Game.eShots.add(new Enemy_Shot(this.getX() + 3, this.getY() + 11, 6, 3, spriteAtk, dx, dy, 40, 5, 35, "straight"));
 		}
 		if (timeAtk % 80 == 0) {
-			Game.enemies.add(new Base_Enemy(this.getX() - 25, this.getY(), 16, 16, null));
+			Game.eShots.add(new Enemy_Shot(this.getX() + 16, this.getY() + 11, 16, 5, spriteAtk2, 0, 0, 32, 5, 50, "focused"));
 		}
 		if (timeAtk % 160 == 0) {
-			Game.enemies.add(new Eye_Enemy(this.getX() + 25, this.getY(), 16, 16, null));
+			addShield();
 			timeAtk = 0;
 		}
 	}
@@ -98,7 +125,13 @@ public class Boss_Duality extends Enemy{
 		closeAtk();
 		rangeAtk();
 		
-		this.shotDamage();
+		if (shieldActive) {
+			shieldColision();
+		}
+		else {
+			this.shotDamage();
+		}
+	
 		if (this.life <= 0) {
 			die();
 		}
@@ -108,6 +141,10 @@ public class Boss_Duality extends Enemy{
 	public void render(Graphics g) {
 		g.drawImage(aura, this.getX() - Camera.x - 32, this.getY() - Camera.y - 32, 98, 98,null);
 		g.drawImage(this.animation[index], this.getX() - Camera.x, this.getY() - Camera.y, null);
+		
+		if (shieldActive) {
+			g.drawImage(shield, this.getX() - Camera.x - 32, this.getY() - Camera.y - 48, 98, 80, null);
+		}
 		
 		g.setColor(new Color(30, 30, 30));
 		g.fillRect((this.getX() + 1) - Camera.x, (this.getY() - 6) - Camera.y, 32, 4);
---
+++ DumbSouls/src/entities/Boss_Hive.java
@@ -0,0 +1,109 @@
+package entities;
+
+import java.awt.image.BufferedImage;
+import java.awt.Color;
+import java.awt.Graphics;
+import main.Game;
+import world.Camera;
+import world.World;
+
+public class Boss_Hive extends Enemy{
+	
+	private int frames, maxFrames = 40, index, maxIndex = 3, timeAtk, spawnX, spawnY, contPosition;
+	private boolean balance;
+			
+	public Boss_Hive(int x, int y, int width, int height, BufferedImage sprite) {
+		super(x, y, width, height, sprite);
+		this.spawnX = x;
+		this.spawnY = y;
+		this.getAnimation(160, 192, 32, 32, 3);
+		this.expValue = 1800;
+		this.soulValue = 30;
+		this.maxLife = 600;
+		this.life = maxLife;
+		this.setMask(2, 4, 30, 28);
+	}
+	
+	private void animate() {
+		frames++;
+		if (frames == maxFrames) {
+			frames = 0;
+			index++;
+			if (index == maxIndex) {
+				index = 0;
+			}
+		}
+	}
+	
+	private void attack() {
+		timeAtk ++;
+		if (timeAtk % 120 == 0) {
+			Game.enemies.add(new Base_Enemy(this.getX() + 80, this.getY() + 60, 16, 16, null));
+			Game.enemies.add(new Base_Enemy(this.getX() - 80, this.getY() + 60, 16, 16, null));
+			Game.enemies.add(new Base_Enemy(this.getX() + 16, this.getY() - 60, 16, 16, null));
+		}
+		
+		if (timeAtk % 160 == 0) {
+			Game.enemies.add(new Eye_Enemy(this.getX() + 80, this.getY() - 60, 16, 16, null));
+			Game.enemies.add(new Eye_Enemy(this.getX() - 80, this.getY() - 60, 16, 16, null));
+		}
+		
+		if (timeAtk % 240 == 0) {
+			Game.enemies.add(new Mouth_Enemy(this.getX() + 16, this.getY() + 60, 16, 16, null));
+		}
+		
+		if (timeAtk % 320 == 0) {
+			Game.enemies.add(new Base_Enemy(Game.player.getX() + 80, Game.player.getY() + 40, 16, 16, null));
+			Game.enemies.add(new Base_Enemy(Game.player.getX() - 80, Game.player.getY() + 40, 16, 16, null));
+			Game.enemies.add(new Base_Enemy(Game.player.getX() + 8, Game.player.getY() - 40, 16, 16, null));
+		}
+	}
+	
+	private void balanceStatus() {
+		this.maxLife =  800 * World.wave / 10;
+		this.expValue = 1500 * World.wave / 10;
+		this.soulValue = 20 * World.wave / 10; 
+		this.life = this.maxLife;
+		balance = true;
+	}
+	
+	private void die() {
+		Game.enemies.remove(this);
+		Game.player.exp += this.expValue;
+		Player.souls +=  this.soulValue;
+	}
+	
+	public void tick() {
+		if (!balance) {
+			balanceStatus();
+		}
+		animate();
+		attack();
+		this.shotDamage();
+		 
+		if (this.getX() != spawnX && this.getY() != spawnY) {
+			contPosition++;
+			if (contPosition == 120) {
+				this.x = spawnX;
+				this.y = spawnY;
+			}
+		}
+		 
+		if (this.life <= 0) {
+			this.die();
+		}
+	}
+	
+	public void render(Graphics g) {
+		g.drawImage(this.animation[index], this.getX() - Camera.x, this.getY() - Camera.y, null);
+		
+		g.setColor(new Color(30, 30, 30));
+		g.fillRect((this.getX() + 1) - Camera.x, (this.getY() - 6) - Camera.y, 32, 4);
+		
+		g.setColor(Color.black);
+		g.fillRect((this.getX() + 2) - Camera.x, (this.getY() - 5) - Camera.y, 30, 2);
+		
+		g.setColor(new Color(125, 23, 145));
+		g.fillRect((this.getX() + 2) - Camera.x, (this.getY() - 5) - Camera.y, (int)((this.life * 30) / maxLife), 2);
+	}
+}
