d31e669b4884a751ba9bbdf2c0901c0297b0a216
Doomer_O <109368541+DoomerO@users.noreply.github.com>
2023-01-24 13:10:25 -0300

    Add files via upload

--- DumbSouls/src/sounds/SoundPlayer.java
+++ DumbSouls/src/sounds/SoundPlayer.java
@@ -7,38 +7,34 @@ import javax.sound.sampled.AudioSystem;
 
 public class SoundPlayer{
 	
-	public static void PlaySound(String sound) {
+	private Clip clip;
+	public SoundPlayer(String sound) {
 		try {
 			File file = new File("res/sounds/" + sound);
 			if (file.exists()) {
-				AudioInputStream audioInp = AudioSystem.getAudioInputStream(file.toURI().toURL());
-				Clip clip = AudioSystem.getClip();
+				AudioInputStream audioInp = AudioSystem.getAudioInputStream(file);
+				clip = AudioSystem.getClip();
 				clip.open(audioInp);
-				clip.start();
 			}
 			else {
-				System.out.println("!!!This sound file does not exists: " + sound);
+				throw new RuntimeException("This file does not exists!!!" + sound);
 			}
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
 	
-	public static void PlaySoundTrack(String music) {
-		try {
-			File file = new File("res/sounds/" + music);
-			if (file.exists()) {
-				AudioInputStream audioInp = AudioSystem.getAudioInputStream(file.toURI().toURL());
-				Clip clip = AudioSystem.getClip();
-				clip.open(audioInp);
-				clip.start();
-				clip.loop(Clip.LOOP_CONTINUOUSLY);
-			}
-			else {
-				System.out.println("!!!This sound file does not exists: " + music);
-			}
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
+	
+	public void play() {
+		clip.setFramePosition(0);
+		clip.start();
+	}
+	
+	public void loop() {
+		clip.loop(Clip.LOOP_CONTINUOUSLY);
+	}
+	
+	public void stop() {
+		clip.stop();
 	}
 }
